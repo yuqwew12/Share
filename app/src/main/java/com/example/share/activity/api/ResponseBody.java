c222d391d7cb9b3dcb31dd4904a3ac02573f79bd
ycy <email>
2024-09-06 18:32:49 +0800

    导航栏

---
+++ app/src/main/java/com/example/share/activity/api/ResponseBody.java
@@ -0,0 +1,32 @@
+package com.example.share.activity.api;
+
+import androidx.annotation.NonNull;
+
+/**
+ * http响应体的封装协议
+ * @param <T> 泛型
+ */
+public class ResponseBody <T> {
+    private int code;
+    private String msg;
+    private T data;
+    public ResponseBody(){}
+    public int getCode() {
+        return code;
+    }
+    public String getMsg() {
+        return msg;
+    }
+    public T getData() {
+        return data;
+    }
+    @NonNull
+    @Override
+    public String toString() {
+        return "ResponseBody{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
